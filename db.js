74250286ae2e81f0e047569a883836ff9eba75dd
DevTinaTi <tinadesk8@gmail.com>
2023-09-15 23:41:30 +0100

    add new ile

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, '.', ".env") });
 const mongoose = require("mongoose");
 
-const Db = () => {
+const Db = async() => {
   mongoose
     .connect(process.env.MONGOURL, {
       useNewUrlParser: true,
