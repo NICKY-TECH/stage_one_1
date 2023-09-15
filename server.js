00fef41233a7ed654a37b77e1d421cd520aecf9a
DevTinaTi <tinadesk8@gmail.com>
2023-09-15 22:48:45 +0100

    add new ile

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT | 3000;
 
 app.listen(PORT, () => {
   console.log(`server currently running at ${PORT}`);
